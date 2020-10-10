bbdc2d6f16b1b3c7582a24f96cd96fb328135a88
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-10 17:19:38 +0530

    Updates

---
+++ precache-manifest.8e67ecce864f23d7b1a0ef1b28b70381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce751794a44dad8534f50f8126cc6e0",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "a1c7c0428be100d47821",
+    "url": "/covid-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "21ad19cdf164284cbc81",
+    "url": "/covid-tracker/static/css/main.4f05d62d.chunk.css"
+  },
+  {
+    "revision": "a1c7c0428be100d47821",
+    "url": "/covid-tracker/static/js/2.49df14d8.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid-tracker/static/js/2.49df14d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ad19cdf164284cbc81",
+    "url": "/covid-tracker/static/js/main.30a7f0ad.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
