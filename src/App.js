549759de16ab50984c2d8f333fe7e61e7b4ff23e
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-10 15:25:10 +0530

    added API call to populate countries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./styles/app.css";
 
-const App =()=>{
-    return <div>
+const App = () => {
+  const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    const getCountryData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountryData();
+  }, [countries]);
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => {
+              return <MenuItem value={country.value}>{country.name}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
-}
+  );
+};
 
-export default App
+export default App;
